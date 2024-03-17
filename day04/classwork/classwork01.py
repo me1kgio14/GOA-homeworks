395386f74070e0a1db7cfc7836001ff48dcd432f
me1kgio <giogiorgi1279@gmail.com>
2024-03-18 00:24:59 +0400

    fifth commit

---
+++ day04/classwork/classwork01.py
@@ -0,0 +1,16 @@
+# my_name='Giorgi'
+# in python underscore's name is snakecase
+#myName='Giorgi'
+#in python this kind of text called camelcase
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ day04/day02/homework0.py
@@ -0,0 +1,20 @@
+#my family
+
+# grandmother
+print('ლიანა ლაფაჩი')
+
+# mother
+print('ელენე კერესელიძე')
+
+# father
+print('ზვიად მელქაძე')
+
+# sister_1
+print('ანასტასია მელქაძე')
+
+# sister_2
+print('ნენე მელქაძე')
+
+#me
+print('გიორგი მელქაძე')
+
