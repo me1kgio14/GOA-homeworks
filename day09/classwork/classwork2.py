4e0f289ebb71f332a0da5e7c894cf9ff39abd09d
me1kgio <giogiorgi1279@gmail.com>
2024-03-18 00:32:47 +0400

    tanth commit

---
+++ day09/classwork/classwork2.py
@@ -0,0 +1,11 @@
+for i in range(1,31):
+    print(i)
+
+for i in range(1,11):
+    print(i*i)
+
+for i in range(1,11):
+    print(i)
+
+for i in range():
+    print(i/2)
---
+++ day09/homework/homework0.py
@@ -0,0 +1,24 @@
+for i in range(21):
+    print(i)
+
+for i in range(int(input("enter number")),int(input("enter second number"))):
+    print(i)
+
+for i in range(50,101):
+    print(i)
+
+for i in range(101,50-1):
+    print(i)
+
+result=0
+for i in range(51):
+   result=result+i
+print(result)
+
+for i in range(0,int(input("enter number"))):
+    print(i)
+
+age=int(input("please enter your age"))
+age10=age+10
+for i in range (age,age10):
+    print(i)
