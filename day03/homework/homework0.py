e162ba519fe41bc79491bc4d95aaa5e4f12909c3
me1kgio <giogiorgi1279@gmail.com>
2024-03-18 00:20:26 +0400

    third commit

--- day03/homework/homework0.py
+++
@@ -1,7 +0,0 @@
-nikas_aplles=6
-sandros_aplles=9
-print(nikas_aplles+sandros_aplles)
-print(nikas_aplles-sandros_aplles)
-print(sandros_aplles-nikas_aplles)
-print(sandros_aplles/nikas_aplles)
-print(nikas_aplles*sandros_aplles)
