3641951a036a938d8b25994a3e19ba7f0e37204f
me1kgio <giogiorgi1279@gmail.com>
2024-03-18 00:30:07 +0400

    eighth commit

---
+++ day07/classwork/classwork1.py
@@ -0,0 +1,5 @@
+name = "saba"
+NAME = "luka"
+Name = "gio"
+print(name,NAME,Name)
+#python is case sensitive language
---
+++ day07/homework/homework0.py
@@ -0,0 +1,11 @@
+box1=int(input("please enter amount of aplles:"))
+box2=int(input("plrase enter amount of carrots:"))
+
+print(box1>box2 or box1<box2)
+print(box1>box2 or box1>box2)
+
+box3=int(input("please enter amount of aplles:"))
+box4=int(input("plrase enter amount of carrots:"))
+
+print(box1>box2 and box1<box2)
+print(box1>box2 and box1>box2)
